7e496011117935478e8314e13384fc3c946042c8
manisankared <your-email@example.com>
2025-10-26 23:30:09 +0530

    Initial commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -255,7 +255,7 @@ const AboutPage = () => {
       </div>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1N8398Rx0nrhwIHgejb7Yp_7plA0rpLQk/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1ucGTeow0a6FXAwEeDZ0XGLNkmkn0iMVm/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
